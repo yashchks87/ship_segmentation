a3ea37b3f25af05a177eff8c25ad7ba312d9ab45
yashchks87 <yashchks87@gmail.com>
2022-12-20 13:43:20 +0000

    Shell script and notebook changes are saved.

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 pip install kaggle
 
+mkdir scripts/
+
+cd scripts
+
 git clone https://github.com/yashchks87/helper_functions_cv.git
 
 export KAGGLE_USERNAME=yashchoksi16
-export KAGGLE_KEY=f355b838448de9fb224e6fe224b8d459
+export KAGGLE_KEY=5cdc24fa6c8455b77f63fbbae4c4179c
+
+cd ../../
 
 kaggle competitions download -c airbus-ship-detection
 
